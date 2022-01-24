57180562be174261c6308764633951c3b8e27c23
marcoret74 <marcoret74@gmail.com>
2022-01-24 14:55:59 -0300

    mas cambios

--- backend/src/main/java/cl/marco/employees/backend/controllers/EmployeeController.java
+++ backend/src/main/java/cl/marco/employees/backend/controllers/EmployeeController.java
@@ -1,12 +1,17 @@
 package cl.marco.employees.backend.controllers;
 
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,7 +44,32 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees")
-    public Employee createEmployee(@Validated @RequestBody Employee employee) {
+    public Employee createEmployee(@Valid @RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
+            @Valid @RequestBody Employee emploeeDetails) throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        employee.setEmailId(emploeeDetails.getEmailId());
+        employee.setEmailId(emploeeDetails.getLastName());
+        employee.setEmailId(emploeeDetails.getFirstName());
+
+        final Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
